a595dfe4ca8e2018bd7c50d1af776bc48598c03c
Quang Anh Nguyen <50756647+quang-anh-nguyen@users.noreply.github.com>
2022-03-18 14:40:28 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,10 +12,10 @@ python cli.py \
 \
 --pet_per_gpu_eval_batch_size 8 \
 --pet_per_gpu_train_batch_size 2 \
---pet_gradient_accumulation_steps 8 \
+--pet_gradient_accumulation_steps 16 \
 --pet_max_steps 250 \
 --pet_max_seq_length 256 \
---pet_repetitions 3 \
+--pet_repetitions 2 \
 --sc_per_gpu_train_batch_size 2 \
 --sc_per_gpu_unlabeled_batch_size 2 \
 --sc_gradient_accumulation_steps 8 \

69b94820beb18cf316220f54022b2ad8507518a0
Quang Anh Nguyen <50756647+quang-anh-nguyen@users.noreply.github.com>
2022-04-07 20:11:00 +0200

    Update tasks.py

--- pet/tasks.py
+++ pet/tasks.py
@@ -558,6 +558,7 @@ class BoolQProcessor(DataProcessor):
                 if 'idx' in example_json.keys():
                     idx = example_json['idx']
                 label = str(example_json['label']) if 'label' in example_json else None
+                label = str(example_json['answer']) if 'answer' in example_json else None
                 guid = "%s-%s" % (set_type, idx)
                 text_a = example_json['passage']
                 text_b = example_json['question']
